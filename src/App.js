cefedbff44a85ab320e9e1dad569f9e9681ececd
AbdulSamii Ajala <jalasem@users.noreply.github.com>
2020-09-20 16:02:30 +0100

    feat: tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./styles.css";
+import "./styles.scss";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App bg-red-500">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
